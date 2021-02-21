2fbe30e4d20a23508f7f36cf6bd3b9f91d4561f8
Александр Ягудин <yagudin.a@outlook.com>
2021-02-21 21:21:37 +0300

    21.02.2021 21:21

---
+++ Supply Admin/Models/Additianal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supply_Admin.Models
+{
+    public class Additianal
+    {
+        //Дополнительные соглашения к договору
+        public int ID { get; set; }//ID
+        
+    }
+}
