fc2709795bfa7b5712b34807f82745b6e55757bf
Александр Ягудин <yagudin.a@outlook.com>
2022-01-20 16:39:40 +0300

    Edited payment orders

--- Supply/Libs/OrdersCreation.cs
+++ Supply/Libs/OrdersCreation.cs
@@ -1800,6 +1800,9 @@ namespace Supply.Libs
                 {
                     SpecialDateCheck(startPaymentDate, endPaymentDate, out days, out monthes, out daysInMonth);
 
+                    if (days != 0)
+                        days += 1;
+
                     CalculationServiceCoast(days, monthes, daysInMonth, ref rent, ref house, ref service, ref electricity);
 
                     return;
