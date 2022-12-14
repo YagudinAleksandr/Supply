ce7cadfab38f1f4d0cf87d4357d55cf46b8af73e
Александр <@USER>
2022-12-14 10:42:41 +0300

    Оплаты

--- Supply/Libs/OrdersCreation.cs
+++ Supply/Libs/OrdersCreation.cs
@@ -1795,7 +1795,8 @@ namespace Supply.Libs
 
                 List<Benefit> benefits = db.Benefits.Where(x => x.OrderID == orderID).ToList();
 
-                
+                SpecialPayments(orderID, out rent, out house, out service);
+                SpecialPaymentsElectricity(orderID, out electricity);
 
                 int days = 0;
                 int daysInMonth = 0;
@@ -1814,8 +1815,7 @@ namespace Supply.Libs
 
                     CalculationServiceCoast(days, monthes, daysInMonth, ref rent, ref house, ref service, ref electricity);
 
-                    SpecialPayments(orderID, out rent, out house, out service);
-                    SpecialPaymentsElectricity(orderID, out electricity);
+                    
 
                     return;
                 }
@@ -1974,8 +1974,7 @@ namespace Supply.Libs
                             rent += tempBenefitRent;
                         }
 
-                        SpecialPayments(orderID, out rent, out house, out service);
-                        SpecialPaymentsElectricity(orderID, out electricity);
+                        
                     }
                 }
             }
